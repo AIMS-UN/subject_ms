4e60470cc2e1f8618ae2d1bfc7bdb27094893e0b
SGman98 <bastiangarnica@gmail.com>
2022-10-22 15:09:22 -0500

    chore: ✨ add db config scripts

---
+++ sql/groups.sql
@@ -0,0 +1,24 @@
+INSERT INTO class_groups (class_group_id, max_capacity, teacher_id)
+VALUES (
+        'cca4c365-befb-4d4b-bd42-d9b92384a57d',
+        30,
+        '2006bc4f-db29-4248-8025-d36348017d2d'
+    );
+INSERT INTO class_groups (class_group_id, max_capacity, teacher_id)
+VALUES (
+        'd0b5c4b5-5b1f-4b2b-9b2c-9b2c9b2c9b2c',
+        30,
+        '2006bc4f-db29-4248-8025-d36348017d2d'
+    );
+INSERT INTO class_groups (class_group_id, max_capacity, teacher_id)
+VALUES (
+        'd0b5c4b5-5b1f-4b2b-9b2c-9b2c9b2c9b2d',
+        30,
+        '2006bc4f-db29-4248-8025-d36348017d2d'
+    );
+INSERT INTO class_groups (class_group_id, max_capacity, teacher_id)
+VALUES (
+        'd0b5c4b5-5b1f-4b2b-9b2c-9b2c9b2c9b2e',
+        30,
+        '2006bc4f-db29-4248-8025-d36348017d2d'
+    );
---
+++ sql/subjects.sql
@@ -0,0 +1,32 @@
+INSERT INTO subjects (career, code, credits, curriculum, name)
+VALUES (
+        1,
+        '1053486524-B',
+        5,
+        'curriculum-1',
+        'Mathematics'
+    );
+INSERT INTO subjects (career, code, credits, curriculum, name)
+VALUES (
+        1,
+        '1053486524-C',
+        5,
+        'curriculum-1',
+        'Physics'
+    );
+INSERT INTO subjects (career, code, credits, curriculum, name)
+VALUES (
+        1,
+        '1053486524-D',
+        5,
+        'curriculum-1',
+        'Chemistry'
+    );
+INSERT INTO subjects (career, code, credits, curriculum, name)
+VALUES (
+        1,
+        '1053486524-E',
+        5,
+        'curriculum-1',
+        'Biology'
+    );
